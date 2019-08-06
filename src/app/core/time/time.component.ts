c3b32c831762524e7e39d350f3ae02ba8099b137
Simonjohanning <Simonjohanning@gmail.com>
2019-08-06 16:04:56 +0200

    Tiem component documentation and observable fix

--- src/app/core/time/time.component.ts
+++ src/app/core/time/time.component.ts
@@ -12,31 +12,43 @@ import {DataProvisionService} from '../data-provision.service';
   templateUrl: './time.component.html',
   styleUrls: ['./time.component.css']
 })
+
+/**
+ * The TimeComponent manages the monitoring of time within the simulation.
+ * It is used to display temporal information within the simulation
+ *
+ */
 export class TimeComponent implements OnInit {
-  private experimentLength: Observable<number>;
-  private experimentLengthNumber;
+  /** Variable to depict the length of the experiment */
+  private experimentLength;
+  /** Variable to store the time within the experiment (how far it progressed) */
   private experimentTime: number;
+  /** Stores the timeRegime (as TimeRegime.DISCRETE or TimeRegime.CONTINUOUS) in order to show the respective component */
   private timeRegime;
   private discreteRegime = TimeRegime.DISCRETE;
   private continousRegime = TimeRegime.CONTINUOUS;
+  /** Variable to contain the date to show progress appropriately */
   private dateView = new Date(0, 0, 0, 0, 0, 0, 0);
+  /** Variable to hold the progress of the simulation */
   private progress = 0;
   constructor(private state: ExperimentStateService,
               private edmLayer: MockEDMService,
-              private timeService: TimeService,
-              private helperService: HelperService) { }
+              private timeService: TimeService) { }
 
   ngOnInit() {
-    this.experimentLength = DataProvisionService.getExperimentLength();
-    this.experimentLength.subscribe(time => this.experimentLengthNumber = time);
+    // Set the length of the experiment with the data obtained from the data provision service
+    DataProvisionService.getExperimentLength().subscribe(time => this.experimentLength = time);
+    // Obtain the current time from the time service
     this.experimentTime = this.timeService.getCurrentTime();
+    // subscribe to the time emitter of the time service to receive updates on the simulation time
     this.timeService.timeEmitter.subscribe(newTime => {
       this.experimentTime = newTime;
-      this.progress = this.experimentTime / this.experimentLengthNumber;
+      this.progress = this.experimentTime / this.experimentLength;
       // TODO find better way for updating date than this
-      this.dateView = new Date(0, 0, 0, 0, 0, 0, 0);
+      // this.dateView = new Date(0, 0, 0, 0, 0, 0, 0);
       this.dateView.setSeconds(newTime);
     });
+    // Set the time regime the simulation is configured with
     DataProvisionService.getTimeRegime().subscribe(regime => this.timeRegime = regime);
   }
 }

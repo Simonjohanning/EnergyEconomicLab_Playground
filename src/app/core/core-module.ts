19eefb65200ac889e427a4b479508edd2944fc7f
Nunca131 <nancyretzlaff@gmx.de>
2020-01-08 10:55:09 +0100

    added welcome component

--- src/app/core/core-module.ts
+++ src/app/core/core-module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-// import { WelcomeComponent } from './welcome/welcome.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 import { SharedModule } from '../shared/shared.module';
 // import { TimeComponent } from './time/time.component';
 import { ExperimentStateService } from './experiment-state.service';
 import { TimeService } from './time.service';
 
 @NgModule({
-//  declarations: [WelcomeComponent, TimeComponent],
-  declarations: [],
+  declarations: [
+    WelcomeComponent,
+  //  TimeComponent
+  ],
   imports: [
     CommonModule,
     FormsModule,

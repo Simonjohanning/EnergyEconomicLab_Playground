a97f165e11d199c33d4bdf0f0e26bd5571941bc3
Nunca131 <nancyretzlaff@gmx.de>
2020-01-08 16:20:43 +0100

    text book test of grid operator component

--- src/app/grid-operator/grid-operator.component.spec.ts
+++ src/app/grid-operator/grid-operator.component.spec.ts
@@ -1,8 +1,6 @@
 import {GridOperatorComponent} from './grid-operator.component';
 import {ExperimentStateService} from '../core/experiment-state.service';
-
-let gridOperator: GridOperatorComponent;
-let mockExperimentService: MockExperimentStateService;
+import {ComponentFixture, inject, TestBed} from '@angular/core/testing';
 
 class MockExperimentStateService extends ExperimentStateService {
   experimentID = 123456;
@@ -10,12 +8,48 @@ class MockExperimentStateService extends ExperimentStateService {
 
 describe('Comp: GridOperator', () => {
 
-  beforeEach( () => {
-    mockExperimentService = new MockExperimentStateService();
-    gridOperator = new GridOperatorComponent(this.mockExperimentService);
+  let gridOperatorComponent: GridOperatorComponent;
+  let fixture: ComponentFixture<GridOperatorComponent>;
+  let testBedService: ExperimentStateService
+  let gridOperatorService: ExperimentStateService;
+
+  beforeEach(() => {
+    // refine the test module by declaring the test component
+    TestBed.configureTestingModule({
+      declarations: [GridOperatorComponent],
+      providers: [ExperimentStateService]
+    });
+
+    // Configure the component with another set of Providers
+    TestBed.overrideComponent(
+      GridOperatorComponent,
+      {set: {providers: [{provide: ExperimentStateService, useClass: MockExperimentStateService}]}}
+    );
+
+    // create component and test fixture
+    fixture = TestBed.createComponent(GridOperatorComponent);
+
+    // get test component from the fixture
+    gridOperatorComponent = fixture.componentInstance;
+
+    // ExperimentStateService provided to the TestBed
+    testBedService = TestBed.get(ExperimentStateService);
+
+    // ExperimentStateService provided by the GridOperatorComponent (should return MockExperimentStateService)
+    gridOperatorService = fixture.debugElement.injector.get(ExperimentStateService);
   });
 
-  it('check whether the gridOperator exists', () => {
-    expect(gridOperator).toBeTruthy();
+  it('GridOperatorComponent exists', () => {
+    expect(gridOperatorComponent).toBeTruthy();
+  });
+
+  it('Service injected via inject(...) and TestBed.get(...) should be the same instance',
+    inject([ExperimentStateService], (injectService: ExperimentStateService) => {
+      expect(injectService).toBe(testBedService);
+    })
+  );
+
+  it('Service injected via component should be an instance of MockExperimentStateService', () => {
+    expect(gridOperatorService instanceof MockExperimentStateService).toBeTruthy();
   });
 });

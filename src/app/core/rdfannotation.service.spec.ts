c095a506921ad6c574754d93ccfb2d7dc1bb8cd2
Nunca131 <nancyretzlaff@gmx.de>
2020-01-08 13:24:48 +0100

    cleaned up

--- src/app/core/rdfannotation.service.spec.ts
+++ src/app/core/rdfannotation.service.spec.ts
@@ -12,27 +12,32 @@ describe('Service: rdfannotations', () => {
     rdfAnnotationService = new RdfannotationService();
   });
 
-  it('should create a primer', () => {
-    expect(RDFAnnotationService.createPrimer(['dct'])).toEqual('@prefix dct:\t <http://purl.org/dc/terms/> .\n\n');
-    expect(RDFAnnotationService.createPrimer(['dcat'])).toEqual('@prefix dcat:\t <http://www.w3.org/ns/dcat#> .\n\n');
-    expect(RDFAnnotationService.createPrimer(['org'])).toEqual('@prefix org:\t <http://www.w3.org/ns/org#> .\n\n');
-    expect(RDFAnnotationService.createPrimer(['rdf'])).toEqual('@prefix rdf:\t <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n\n');
-    expect(RDFAnnotationService.createPrimer(['rdfs'])).toEqual('@prefix rdfs:\t <http://www.w3.org/2000/01/rdf-schema#> .\n\n');
-    expect(RDFAnnotationService.createPrimer(['owl'])).toEqual('@prefix owl:\t <http://www.w3.org/2002/07/owl#> .\n\n');
-    expect(RDFAnnotationService.createPrimer(['xsd'])).toEqual('@prefix xsd:\t <http://www.w3.org/2001/XMLSchema#> .\n\n');
-    expect(RDFAnnotationService.createPrimer(['skos'])).toEqual('@prefix skos:\t <http://www.w3.org/2004/02/skos/core#> .\n\n');
-    expect(RDFAnnotationService.createPrimer(['foaf'])).toEqual('@prefix foaf:\t <http://xmlns.com/foaf/0.1/> .\n\n');
-    expect(RDFAnnotationService.createPrimer(['iirm'])).toEqual('@prefix iirm:\t <http://www.wifa.uni-leipzig.de/iirm/> .\n\n');
-    expect(RDFAnnotationService.createPrimer(['vcard'])).toEqual('@prefix vcard:\t <http://www.w3.org/2006/vcard/ns#> .\n\n');
-    expect(RDFAnnotationService.createPrimer(['eur'])).toEqual('@prefix eur:\t <http://publications.europa.eu/resource/authority/> .\n\n');
-    expect(RDFAnnotationService.createPrimer(['dcatde'])).toEqual('@prefix dcatde:\t <http://dcat-ap.de/def/dcatde/> .\n\n');
-    expect(() => {RDFAnnotationService.createPrimer(['blubb']); }).toThrow(new Error('vocabulary blubb is not implemented in annotation! Will create invalid RDF!!'));
+  describe('should create the correct primer', () => {
+    it ('testing singleton', () => {
+      expect(RDFAnnotationService.createPrimer(['dct'])).toEqual('@prefix dct:\t <http://purl.org/dc/terms/> .\n\n');
+      expect(RDFAnnotationService.createPrimer(['dcat'])).toEqual('@prefix dcat:\t <http://www.w3.org/ns/dcat#> .\n\n');
+      expect(RDFAnnotationService.createPrimer(['org'])).toEqual('@prefix org:\t <http://www.w3.org/ns/org#> .\n\n');
+      expect(RDFAnnotationService.createPrimer(['rdf'])).toEqual('@prefix rdf:\t <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n\n');
+      expect(RDFAnnotationService.createPrimer(['rdfs'])).toEqual('@prefix rdfs:\t <http://www.w3.org/2000/01/rdf-schema#> .\n\n');
+      expect(RDFAnnotationService.createPrimer(['owl'])).toEqual('@prefix owl:\t <http://www.w3.org/2002/07/owl#> .\n\n');
+      expect(RDFAnnotationService.createPrimer(['xsd'])).toEqual('@prefix xsd:\t <http://www.w3.org/2001/XMLSchema#> .\n\n');
+      expect(RDFAnnotationService.createPrimer(['skos'])).toEqual('@prefix skos:\t <http://www.w3.org/2004/02/skos/core#> .\n\n');
+      expect(RDFAnnotationService.createPrimer(['foaf'])).toEqual('@prefix foaf:\t <http://xmlns.com/foaf/0.1/> .\n\n');
+      expect(RDFAnnotationService.createPrimer(['iirm'])).toEqual('@prefix iirm:\t <http://www.wifa.uni-leipzig.de/iirm/> .\n\n');
+      expect(RDFAnnotationService.createPrimer(['vcard'])).toEqual('@prefix vcard:\t <http://www.w3.org/2006/vcard/ns#> .\n\n');
+      expect(RDFAnnotationService.createPrimer(['eur'])).toEqual('@prefix eur:\t <http://publications.europa.eu/resource/authority/> .\n\n');
+      expect(RDFAnnotationService.createPrimer(['dcatde'])).toEqual('@prefix dcatde:\t <http://dcat-ap.de/def/dcatde/> .\n\n');
+      expect(() => {RDFAnnotationService.createPrimer(['blubb']); }).toThrow(new Error('vocabulary blubb is not implemented in annotation! Will create invalid RDF!!'));
+    });
 
-    expect(RDFAnnotationService.createPrimer(['dcat', 'skos', 'dct'])).toEqual('@prefix dcat:\t <http://www.w3.org/ns/dcat#> .\n' +
-      '@prefix skos:\t <http://www.w3.org/2004/02/skos/core#> .\n' + '@prefix dct:\t <http://purl.org/dc/terms/> .\n\n');
-
-    // throws error for first wrong input
-    expect(() => {RDFAnnotationService.createPrimer(['vcard', 'scos', 'faf']); }).toThrow(new Error('vocabulary scos is not implemented in annotation! Will create invalid RDF!!'));
+    it ('should create the primers in correct order', () => {
+      expect(RDFAnnotationService.createPrimer(['dcat', 'skos', 'dct'])).toEqual('@prefix dcat:\t <http://www.w3.org/ns/dcat#> .\n' +
+        '@prefix skos:\t <http://www.w3.org/2004/02/skos/core#> .\n' + '@prefix dct:\t <http://purl.org/dc/terms/> .\n\n');
+    });
+    it ('should throw an error for the first unrecognized input', () => {
+      // throws error for first wrong input
+      expect(() => {RDFAnnotationService.createPrimer(['vcard', 'scos', 'faf']); }).toThrow(new Error('vocabulary scos is not implemented in annotation! Will create invalid RDF!!'));
+    });
   });
 
   it('should return author information', () => {

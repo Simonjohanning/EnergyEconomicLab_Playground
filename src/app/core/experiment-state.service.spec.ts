1ee4cac3006c111c7e252b806027256ed13897b2
Nunca131 <nancyretzlaff@gmx.de>
2020-01-08 13:21:57 +0100

    fixed an unhandled exeption

---
+++ src/app/core/experiment-state.service.spec.ts
@@ -0,0 +1,24 @@
+
+import {ExperimentStateService} from './experiment-state.service';
+import {DataProvisionService} from './data-provision.service';
+
+describe('ExperimentStateService', () => {
+  let experimentStateService: ExperimentStateService;
+
+  beforeEach( () => {
+    experimentStateService = new ExperimentStateService();
+  });
+
+  it('current prosumer should be the first prosumer created in the data provision service', () => {
+    DataProvisionService.getProsumerData(1).subscribe(prosumerInstance => {
+      expect(experimentStateService.getCurrentProsumer()).toEqual(prosumerInstance);
+    });
+  });
+
+  it('set current prosumer should accept new prosumer if it was different than the one in storage', () => {
+    DataProvisionService.getProsumerData(1).subscribe(prosumerInstance => {
+      expect(experimentStateService.setCurrentProsumer(prosumerInstance)).toEqual(true);
+    });
+  });
+
+});
---
+++ src/app/core/experiment-state.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { DataProvisionService } from './data-provision.service';
+import { ProsumerInstance } from './data-types/ProsumerInstance';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+// TODO Include / think about registering for experiments, signal readiness and handling experimental data
+// TODO think if session data considering the respective prosumer should be stored here
+/**
+ * This service holds and provides stateful information about an experiment (instance) to respective agents.
+ *
+ */
+export class ExperimentStateService {
+  /** The prosumer instance participating in the experiment */
+  private currentProsumer: ProsumerInstance;
+  /** The id of experiment that is conducted */
+  public experimentID: number;
+  /** ID of the prosumer in order to derive the respective prosumer */
+  public actorID: number;
+  /** The prosumer (instances) participating in the experiment */
+  private prosumers: ProsumerInstance[];
+
+  constructor() {
+    DataProvisionService.getProsumerData(this.actorID).subscribe(prosumerInstance => {
+      this.currentProsumer = prosumerInstance;
+    //  this.prosumers.push(this.currentProsumer);
+    });
+  }
+
+  getCurrentProsumer(): ProsumerInstance {
+    return this.currentProsumer;
+  }
+
+  setCurrentProsumer(prosumerToSet: ProsumerInstance): boolean {
+    if (this.prosumers === undefined) {
+      this.prosumers = new Array(prosumerToSet);
+      return true;
+    }
+    if (!this.prosumers.includes(prosumerToSet)) { return false; } else {
+      this.currentProsumer = prosumerToSet;
+      return true;
+    }
+  }
+
+}

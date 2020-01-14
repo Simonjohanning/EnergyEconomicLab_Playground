7b3f3a18ed54d608846664a1d20f07d537943fa5
Nunca131 <nancyretzlaff@gmx.de>
2020-01-14 15:10:48 +0100

    time component and unit tests

---
+++ src/app/core/time/time.component.spec.ts
@@ -0,0 +1,126 @@
+import { ExperimentStateService } from '../experiment-state.service';
+import { MockEDMService } from '../mock-edm.service';
+import { TimeService } from '../time.service';
+import { ComponentFixture, fakeAsync, inject, TestBed, tick } from '@angular/core/testing';
+import { TimeComponent } from './time.component';
+import {DataProvisionService} from '../data-provision.service';
+import {By} from '@angular/platform-browser';
+import {timeInterval} from 'rxjs/operators';
+
+class MockExperimentStateService extends ExperimentStateService {}
+
+class MockedMockEDMService extends MockExperimentStateService {}
+
+class MockTimeService extends TimeService {}
+
+describe('Component: Time', () => {
+
+  let timeComponent: TimeComponent;
+  let fixture: ComponentFixture<TimeComponent>;
+  let testBedExperimentStateService: ExperimentStateService;
+  let timeComponentExperimentStateService: ExperimentStateService;
+  let testBedMockEDMService: MockEDMService;
+  let timeComponentMockEDMService: MockEDMService;
+  let testBedTimeService: TimeService;
+  let timeComponentTimeService: TimeService;
+
+  beforeEach(() => {
+    // refine the test module by declaring the test component
+    TestBed.configureTestingModule({
+      declarations: [TimeComponent],
+      providers: [
+        ExperimentStateService,
+        MockEDMService,
+        TimeService
+      ]
+    });
+    // Configure the component with another set of Providers
+    TestBed.overrideComponent(
+      TimeComponent,
+      {set: {providers: [
+            {provide: ExperimentStateService, useClass: MockExperimentStateService},
+            {provide: MockEDMService, useClass: MockedMockEDMService},
+            {provide: TimeService, useClass: MockTimeService}
+            ]}});
+
+    // create component and test fixture
+    fixture = TestBed.createComponent(TimeComponent);
+    fixture.detectChanges();
+
+    // get test component from the fixture
+    timeComponent = fixture.componentInstance;
+
+    // ExperimentStateService provided to the TestBed
+    testBedExperimentStateService = TestBed.get(ExperimentStateService);
+
+    // ExperimentStateService provided by the TimeComponent (should return MockExperimentStateService)
+    timeComponentExperimentStateService = fixture.debugElement.injector.get(ExperimentStateService);
+
+    // MockEDMService provided to the TestBed
+    testBedMockEDMService = TestBed.get(MockEDMService);
+
+    // MockEDMService provided by the TimeComponent (should return MockedMockEDMService)
+    timeComponentMockEDMService = fixture.debugElement.injector.get(MockEDMService);
+
+    // TimeService provided to the TestBed
+    testBedTimeService = TestBed.get(TimeService);
+
+    // TimeService provided by the TimeComponent (should return MockTimeService)
+    timeComponentTimeService = fixture.debugElement.injector.get(TimeService);
+  });
+
+  it('TimeComponent exists', () => {
+    expect(timeComponent).toBeTruthy();
+  });
+
+  it('Service injected via inject(...) and TestBed.get(...) should be the same instance for ExperimentStateService',
+    inject([ExperimentStateService], (injectService: ExperimentStateService) => {
+      expect(injectService).toBe(testBedExperimentStateService);
+    })
+  );
+
+  it('Service injected via component should be an instance of MockExperimentStateService', () => {
+    expect(timeComponentExperimentStateService instanceof MockExperimentStateService).toBeTruthy();
+  });
+
+  it('Service injected via inject(...) and TestBed.get(...) should be the same instance for MockEDMService',
+    inject([MockEDMService], (injectService: MockEDMService) => {
+      expect(injectService).toBe(testBedMockEDMService);
+    })
+  );
+
+  it('Service injected via component should be an instance of MockedMockEDMService', () => {
+    expect(timeComponentMockEDMService instanceof MockedMockEDMService).toBeTruthy();
+  });
+
+  it('Service injected via inject(...) and TestBed.get(...) should be the same instance for TimeService',
+    inject([TimeService], (injectService: TimeService) => {
+      expect(injectService).toBe(testBedTimeService);
+    })
+  );
+
+  it('Service injected via component should be an instance of MockTimeService', () => {
+    expect(timeComponentTimeService instanceof MockTimeService).toBeTruthy();
+  });
+
+  // as of now it is impossible to set the time regime to anything but discrete - omitting continuous
+  it('should have the correct time and progress displayed when time regime is discrete', () => {
+    let length: number;
+    DataProvisionService.getExperimentLength().subscribe(time => length = time);
+    let actualTime = 0;
+
+    // timeComponentTimeService = fixture.debugElement.injector.get(TimeService);
+    // TimeService provided by the TimeComponent (should return MockTimeService)
+    timeComponentTimeService.timeEmitter.subscribe(newTime => {
+      actualTime = newTime;
+    });
+    const element = fixture.debugElement.query(By.css('.timeDisplay'));
+    expect(element.nativeElement.textContent.trim()).toEqual('discrete time:' + actualTime + ' /' + length);
+    expect(timeComponent.progress).toEqual(actualTime / length);
+
+    // testBedTimeService = TestBed.get(TimeService); TimeService provided to the TestBed
+    testBedTimeService.advanceTime(1);
+    expect(element.nativeElement.textContent.trim()).toEqual('discrete time:' + actualTime + ' /' + length);
+    expect(timeComponent.progress).toEqual(actualTime / length);
+  });
+});
---
+++ src/app/core/time/time.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { ExperimentStateService } from '../experiment-state.service';
+import { MockEDMService } from '../mock-edm.service';
+import { TimeService } from '../time.service';
+import { TimeRegime } from '../data-types/TimeRegime';
+import { DataProvisionService } from '../data-provision.service';
+
+@Component({
+  selector: 'app-time',
+  templateUrl: './time.component.html',
+  styleUrls: ['./time.component.css']
+})
+
+/**
+ * The TimeComponent manages the monitoring of time within the simulation.
+ * It is used to display temporal information within the simulation
+ *
+ */
+export class TimeComponent implements OnInit {
+  /** Variable to depict the length of the experiment */
+  private experimentLength;
+  /** Variable to store the time within the experiment (how far it progressed) */
+  private experimentTime: number;
+  /** Stores the timeRegime (as TimeRegime.DISCRETE or TimeRegime.CONTINUOUS) in order to show the respective component */
+  private timeRegime;
+  private discreteRegime = TimeRegime.DISCRETE;
+  private continuousRegime = TimeRegime.CONTINUOUS;
+  /** Variable to contain the date to show progress appropriately */
+  private dateView = new Date(0, 0, 0, 0, 0, 0, 0);
+  /** Variable to hold the progress of the simulation */
+  private progress = 0;
+  constructor(private state: ExperimentStateService,
+              private edmLayer: MockEDMService,
+              private timeService: TimeService) { }
+
+  ngOnInit() {
+    // Set the length of the experiment with the data obtained from the data provision service
+    DataProvisionService.getExperimentLength().subscribe(time => this.experimentLength = time);
+    // Obtain the current time from the time service
+    this.experimentTime = this.timeService.getCurrentTime();
+    // subscribe to the time emitter of the time service to receive updates on the simulation time
+    this.timeService.timeEmitter.subscribe(newTime => {
+      this.experimentTime = newTime;
+      this.progress = this.experimentTime / this.experimentLength;
+      // TODO find better way for updating date than this
+      // this.dateView = new Date(0, 0, 0, 0, 0, 0, 0);
+      this.dateView.setSeconds(newTime);
+    });
+    // Set the time regime the simulation is configured with
+    DataProvisionService.getTimeRegime().subscribe(regime => this.timeRegime = regime);
+  }
+}

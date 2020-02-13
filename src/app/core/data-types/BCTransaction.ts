c2675809a2062bca065105a279b94e1d853b2ad7
Nunca131 <nancyretzlaff@gmx.de>
2020-02-13 14:28:11 +0100

    now compilable version

---
+++ src/app/core/data-types/BCTransaction.ts
@@ -0,0 +1,18 @@
+import {P2PBid} from './P2PBid';
+import {ProsumerInstance} from './ProsumerInstance';
+
+/**
+ * Data type to represent a transaction to the blockchain
+ *
+ * @param author The prosumer that created the transaction
+ * @param p2pbid The bid on the P2P market the transaction adds to the blockchain
+ * @param timestamp The (current) time the transaction should be timestamped on the respective nodes with
+ */
+export interface BCTransaction {
+  /** The prosumer instance that created the transaction */
+  author: ProsumerInstance;
+  /** The bid on the P2P market the transaction adds to the blockchain */
+  p2pbid?: P2PBid;
+  /** The time the transaction should be timestamped on the respective nodes with */
+  timestamp: number;
+}

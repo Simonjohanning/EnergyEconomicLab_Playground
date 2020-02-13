0bfa0035395c8310187d12e7bccec12be4d84131
Nunca131 <nancyretzlaff@gmx.de>
2020-02-13 10:39:43 +0100

    added small tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,11 +1,27 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {AppComponent} from '../../src/app/app.component';
+import {By} from '@angular/platform-browser';
+import {TimeComponent} from '../../src/app/core/time/time.component';
+import {ExperimentStateService} from '../../src/app/core/experiment-state.service';
+import {MockEDMService} from '../../src/app/core/mock-edm.service';
+import {TimeService} from '../../src/app/core/time.service';
 
 describe('workspace-project App', () => {
   let page: AppPage;
+  let appComp: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
 
   beforeEach(() => {
     page = new AppPage();
+    TestBed.configureTestingModule({
+      declarations: [AppComponent]
+    });
+    TestBed.compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    appComp = fixture.componentInstance;
   });
 
   it('should display welcome message', () => {
@@ -20,4 +36,14 @@ describe('workspace-project App', () => {
       level: logging.Level.SEVERE,
     }));
   });
+
+  it('should create the app', () => {
+    expect(appComp).toBeTruthy();
+  });
+  it('title should be shown', () => {
+    expect(fixture.debugElement.nativeElement.querySelector('h1')).toEqual(appComp.title);
+  });
+
+// TODO research component are all DOM elements of both available? --> if not, in the beginning there should be none, in the end only the respective one (also after clicking around)
 });
+

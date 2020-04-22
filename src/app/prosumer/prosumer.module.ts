993fb60b35679402e52b37c1ccfbd6547508185b
Nunca131 <nancyretzlaff@gmx.de>
2020-04-22 17:57:04 +0200

    pushed bids to own bid view

--- src/app/prosumer/prosumer.module.ts
+++ src/app/prosumer/prosumer.module.ts
@@ -16,36 +16,39 @@ import { MarketViewComponent } from './market-view/market-view.component';
 import { P2PBidEditorComponent } from './p2p-bid-editor/p2p-bid-editor.component';
 import { ResidualLoadComponent } from './residual-load/residual-load.component';
 import { FeedInObligationDisplayComponent } from './feed-in-obligation-display/feed-in-obligation-display.component';
-import { FeeLevyDisplayComponent } from '../prosumer/fee-levy-display/fee-levy-display.component';
-import { AssetDispatchComponent } from '../prosumer/asset-dispatch/asset-dispatch.component';
-import { CGDispatchComponent } from '../prosumer/cgdispatch/cgdispatch.component';
-import { StorageDispatchComponent } from '../prosumer/storage-dispatch/storage-dispatch.component';
-import { LoadDispatchComponent } from '../prosumer/load-dispatch/load-dispatch.component';
+import { FeeLevyDisplayComponent } from './fee-levy-display/fee-levy-display.component';
+import { AssetDispatchComponent } from './asset-dispatch/asset-dispatch.component';
+import { CGDispatchComponent } from './cgdispatch/cgdispatch.component';
+import { StorageDispatchComponent } from './storage-dispatch/storage-dispatch.component';
+import { LoadDispatchComponent } from './load-dispatch/load-dispatch.component';
 import { TransactionClearingService } from '../core/transaction-clearing.service';
 import { BlockchainTransactionService } from '../core/blockchain-transaction.service';
 import { ExogeneousPricesComponent } from './exogeneous-prices/exogeneous-prices.component';
+import { BidViewComponent } from './bid-view/bid-view.component';
 
 @NgModule({
-  declarations: [
-    ProsumerComponent,
-    PersistentResourceDisplayComponent,
-    ControllableGenerationPRDComponent,
-    NonControllableGenerationPRDComponent,
-    LoadPRDComponent,
-    StoragePRDComponent,
-    FeedInPointDisplayComponent,
-    MarketViewComponent,
-    BidDetailComponent,
-    CommittedTransactionsComponent,
-    P2PBidEditorComponent,
-    ResidualLoadComponent,
-    FeedInObligationDisplayComponent,
-    FeeLevyDisplayComponent,
-    AssetDispatchComponent,
-    CGDispatchComponent,
-    StorageDispatchComponent,
-    LoadDispatchComponent,
-    ExogeneousPricesComponent],
+    declarations: [
+        ProsumerComponent,
+        PersistentResourceDisplayComponent,
+        ControllableGenerationPRDComponent,
+        NonControllableGenerationPRDComponent,
+        LoadPRDComponent,
+        StoragePRDComponent,
+        FeedInPointDisplayComponent,
+        MarketViewComponent,
+        BidDetailComponent,
+        CommittedTransactionsComponent,
+        P2PBidEditorComponent,
+        ResidualLoadComponent,
+        FeedInObligationDisplayComponent,
+        FeeLevyDisplayComponent,
+        AssetDispatchComponent,
+        CGDispatchComponent,
+        StorageDispatchComponent,
+        LoadDispatchComponent,
+        ExogeneousPricesComponent,
+        BidViewComponent
+    ],
   imports: [
     CommonModule,
     FormsModule,

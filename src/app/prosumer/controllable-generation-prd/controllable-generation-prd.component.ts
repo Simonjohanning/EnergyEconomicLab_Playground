65e0654006cf4634aec1129851b3b6b61c3f2226
Nunca131 <nancyretzlaff@gmx.de>
2020-05-05 09:51:28 +0200

    minor clean up

--- src/app/prosumer/controllable-generation-prd/controllable-generation-prd.component.ts
+++ src/app/prosumer/controllable-generation-prd/controllable-generation-prd.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ControllableGenerator } from '../../core/data-types/ControllableGenerator';
-import { ProsumerInstance } from '../../core/data-types/ProsumerInstance';
 
 @Component({
   selector: 'app-controllable-generation-prd',

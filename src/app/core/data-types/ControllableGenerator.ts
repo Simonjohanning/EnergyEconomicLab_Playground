acd7d13e0c482a95d507f5f35f662083aa83cb9e
Nunca131 <nancyretzlaff@gmx.de>
2020-05-05 08:04:02 +0200

    clean-up

--- src/app/core/data-types/ControllableGenerator.ts
+++ src/app/core/data-types/ControllableGenerator.ts
@@ -41,67 +41,6 @@ export class ControllableGenerator extends DispatchableAsset {
   // TODO after content checked, set view parameters again
 
   /**
-   * Method that sets the generation of the generator if the requested generation
-   * is within the technical boundaries of the generator.
-   *
-   * @param requestedGeneration The amount of electricity the generator is desired to generate from the current time until changed
-   * @param currentTime The system time at which the generator is to be set
-   */
-  public setGeneration(requestedGeneration: number, currentTime: number): boolean {
-    if (currentTime < this.lastGenerationTime) {
-      return false;
-    } else if (this.checkGeneration(requestedGeneration)) {
-      this.lastGenerationAmount = this.currentGeneration;
-      this.currentGeneration = requestedGeneration;
-      this.lastGenerationTime = currentTime;
-      if (this.operationStatus && (requestedGeneration === 0)) {
-        this.lastOperationStatusSwitch = currentTime;
-        this.operationStatus = false;
-      } else if (!this.operationStatus && (requestedGeneration > 0)) {
-        this.lastOperationStatusSwitch = currentTime;
-        this.operationStatus = true;
-      }
-      this.generationHistory[currentTime] = requestedGeneration;
-      return true;
-    } else { return false; }
-  }
-
-  /**
-   * Method to check whether the asset can be operated at a certain generation level at the next time step
-   *
-   * @param requestedGeneration The generation level to check validity for
-   * @returns boolean value informing whether the requested generation level is technically suitable
-   */
-  private checkGeneration(requestedGeneration: number): boolean {
-    if (this.operationStatus && (requestedGeneration === 0) && (this.minimalUptime < this.lastOperationStatusSwitch)) {
-      return false;
-    } else if (!this.operationStatus && (requestedGeneration > 0) && (this.minimalDowntime < this.lastOperationStatusSwitch)) {
-      return false;
-    } else if (Math.abs(this.currentGeneration - requestedGeneration) > (this.maximalGeneration * this.rampingParameter)) {
-      return false;
-    }
-  }
-
-  /**
-   * Method to return the generation of the asset at a given time point
-   *
-   * @param currentTime The time point at which to check the current generation of the asset
-   * @returns The current generation level (current in the sense of the provided parameter)
-   */
-  public getCurrentGeneration(currentTime: number): number {
-    // still ramping
-    if (Math.abs(currentTime - this.lastGenerationTime) * this.rampingParameter < Math.abs(this.lastGenerationAmount - this.currentGeneration)) {
-      if (this.lastGenerationAmount < this.currentGeneration) {
-        return (this.lastGenerationAmount + (currentTime - this.lastGenerationTime) * this.rampingParameter);
-      } else {
-        return (this.lastGenerationAmount - (currentTime - this.lastGenerationTime) * this.rampingParameter);
-      }
-    } else {
-      return this.currentGeneration;
-    }
-  }
-
-  /**
    * Initiates scheduling and ramping arrays.
    *
    * @param experimentLength The preset time of the experiment

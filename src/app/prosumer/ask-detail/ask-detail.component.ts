07c42ad7cd72248aef79d0d4bc79c5a4f63761a6
Nunca131 <nancyretzlaff@gmx.de>
2020-05-06 10:07:09 +0200

    fixed documentation

--- src/app/prosumer/ask-detail/ask-detail.component.ts
+++ src/app/prosumer/ask-detail/ask-detail.component.ts
@@ -13,8 +13,8 @@ import { TimeService } from '../../core/time.service';
 // TODO allow for toggling to see with and without fees & levies
 
 /**
- * Component to display the details of a P2POption and to provide bid commitment / purchase functionality of the detailed bid.
- * Commitment to the bid emits an event to the parent component to remove the detailed bid from the parent element
+ * Component to display the details of a P2POption and to provide ask commitment / purchase functionality of the detailed ask.
+ * Commitment to the ask emits an event to the parent component to remove the detailed ask from the parent element
  */
 export class AskDetailComponent implements OnInit {
   /** The respective P2POption to detail */
@@ -29,8 +29,8 @@ export class AskDetailComponent implements OnInit {
   }
 
   /**
-   * Method for committing to the detailed bid by the current prosumer (i.e. the one the ExperimentStateService refers to) using the BlockchainTransactionService
-   * Furthermore attempts to remove the component from the parent component if successful, since it doesn't represent a bid anymore
+   * Method for committing to the detailed ask by the current prosumer (i.e. the one the ExperimentStateService refers to) using the BlockchainTransactionService
+   * Furthermore attempts to remove the component from the parent component if successful, since it doesn't represent a ask anymore
    */
   public sell(): void {
     if (this.bts.commitToP2PAsk(this.sessionData.getCurrentProsumer(), this.timeService.getCurrentTime(), this.ask)) {
